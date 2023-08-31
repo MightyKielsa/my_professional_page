3e2d4e78eed6fd220ca0b40b6dc77802746467cf
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2023-08-31 14:16:13 +0100

    update to skills

--- src/data/skillsArr.js
+++ src/data/skillsArr.js
@@ -4,7 +4,7 @@ const skillsArr = [
 		skillImg: '/js.png',
 		skillAlt: 'JS icon',
 		skillId: 1,
-		info: 'I learned JS during the intensive web development bootcamp at School of Code. Most of my projects are currently written in this language',
+		info: 'I learned JS during the intensive web development bootcamp at School of Code. Most of my personal projects are currently written in this language and I had a chance to work with it professionally at Hippo',
 		projectsArr: [
 			{
 				projectName: 'Bootcamp Adiutor',
@@ -31,21 +31,15 @@ const skillsArr = [
 		]
 	},
 	{
-		skillName: 'HTML/CSS',
-		skillImg: '/html.png',
-		skillAlt: 'HTML and CSS icon',
+		skillName: 'Python',
+		skillImg: '/python.png',
+		skillAlt: 'Python icon',
 		skillId: 3,
-		info: 'I got introduced to the basics of HTML and CSS in the secondary school. I decided to re-learn it as a part of my career change plan.',
+		info: 'I used my Python knowledge while working on data integration projects during my time at Hippo Digital and my personal projects, including libraries such as Requests, Pydantic, Boto3, Pytest and more.',
 		projectsArr: [
 			{
-				projectName: 'Bootcamp Adiutor',
+				projectName: 'Chat Statistics Website',
 				projectUrl: 'https://github.com/MightyKielsa/bootcamp_helper_app_project'
-			},
-			{ projectName: 'Weather App', projectUrl: '#' },
-			{ projectName: 'turnUp', projectUrl: 'https://github.com/MightyKielsa/turnUp_front-end' },
-			{
-				projectName: 'This page!',
-				projectUrl: 'https://github.com/MightyKielsa/my_professional_page'
 			}
 		]
 	},
@@ -54,7 +48,7 @@ const skillsArr = [
 		skillImg: '/atom.png',
 		skillAlt: 'React icon',
 		skillId: 4,
-		info: 'School of Code provided me not only with fundamentals of JS, but also allowed me to explore React and concepts necessary for learning other frameworks.',
+		info: 'I have many personal projects built with React and I got a chance to work with this framework and with Next.js while working at Hippo',
 		projectsArr: [
 			{
 				projectName: 'Bootcam Adiutor',
@@ -72,7 +66,7 @@ const skillsArr = [
 		skillImg: '/svelte.png',
 		skillAlt: 'Svelte icon',
 		skillId: 5,
-		info: "I heard about Svelte from a person close to me and I decided to explore it as part of my learning journey. I fell in love with it's simplicity and efficiency",
+		info: "I learned and used Svelte to develop my portfolio page and I fell in love with it's simplicity and efficiency.",
 		projectsArr: [
 			{
 				projectName: 'This page!',
@@ -85,7 +79,7 @@ const skillsArr = [
 		skillImg: '/node-js.png',
 		skillAlt: 'Node icon',
 		skillId: 6,
-		info: 'Since front-end is only half the story, I learned node.js and the magic of the back end at School of Code. I used this framework in all of my full-stack projects',
+		info: 'Since front-end is only half the story, I also know Node.js and used it in many fo my personal and team projects, to maintain the uniformity of the language accross the front and the backend.',
 		projectsArr: [
 			{
 				projectName: 'Bootcam Adiutor',
@@ -99,7 +93,7 @@ const skillsArr = [
 		skillImg: '/sql-server.png',
 		skillAlt: 'SQL icon',
 		skillId: 7,
-		info: 'More specifically PostgreSQL! It was taugth to me as a tool for managing relational databases and me and my teams used it in most of our projects.',
+		info: 'I have a good grasp of relational databases and working experience with SQL. I used PostgresSQL for most of my personal projects. ',
 		projectsArr: [
 			{
 				projectName: 'Bootcam Adiutor',
@@ -113,7 +107,7 @@ const skillsArr = [
 		skillImg: '/amazon-aws.png',
 		skillAlt: 'AWS icon',
 		skillId: 8,
-		info: 'Me and my team used AWS Amplify and Cognito in our project TurnUp to handle deployment and authentication, despite a steep learning curve it is a great tool to work with.',
+		info: 'During my time at Hippo Digital I worked extensively with tools such as: Lambda, DynamoDB, S3, Eventbridge, Step Functions, Cloudwatch and more. In my personal projects I also gained exposure to AWS Amplify and Cognito',
 		projectsArr: [
 			{ projectName: 'turnUp', projectUrl: 'https://github.com/MightyKielsa/turnUp_front-end' }
 		]
@@ -123,7 +117,7 @@ const skillsArr = [
 		skillImg: '/docker.png',
 		skillAlt: 'Docker icon',
 		skillId: 9,
-		info: 'I learned how to set up and use docker for a personal project that my friend offered me to join (repo below).',
+		info: 'I learned how to set up and use docker for a personal project.',
 		projectsArr: [
 			{
 				projectName: 'Groupchat Stats Website',
@@ -132,11 +126,11 @@ const skillsArr = [
 		]
 	},
 	{
-		skillName: 'Jest/Cypress',
+		skillName: 'Unit Testing',
 		skillImg: '/testing.png',
 		skillAlt: 'testing icon',
 		skillId: 10,
-		info: 'I was introduced to the idea of Test Driven Development during my time at SoC bootcamp. They also made me familiar with libraries like Jest and Cypress which I later used in several projects.',
+		info: "I'm experienced in using unit testing libraries such as Pytest and Unittest for Python, and Jest for Javascript. I used those technologies working on a data integration project at Hippo as well as my personal projects ",
 		projectsArr: [
 			{
 				projectName: 'turnUp',
@@ -162,19 +156,15 @@ const skillsArr = [
 		]
 	},
 	{
-		skillName: 'Express',
-		skillImg: '/express.png',
-		skillAlt: 'Express.js icon',
+		skillName: 'Terraform',
+		skillImg: '/terraform.png',
+		skillAlt: 'Terraform icon',
 		skillId: 12,
-		info: 'The fullstack web development bootcamp I completed introduced me to the express.js framework and the concept of REST API which I later used in multiple projects.',
+		info: 'Through my work at Hippo I was exposed to Terraform and learned to deploy cloud architecture with the use of Terraform.',
 		projectsArr: [
 			{
-				projectName: 'turnUp',
-				projectUrl: 'https://github.com/MightyKielsa/turnUp_front-end'
-			},
-			{
-				projectName: 'Bootcamp Adiutor',
-				projectUrl: 'https://github.com/MightyKielsa/bootcamp_adiutor_front_end'
+				projectName: 'Professional Data Integration Project',
+				projectUrl: '#'
 			}
 		]
 	}
